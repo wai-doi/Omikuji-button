f84bd7499697c49f15b2c904a0338efb797bb547
yusuke <>
2018-04-08 23:03:24 +0900

    Display random integer

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@
   var btn = document.getElementById('btn');
 
   btn.addEventListener('click', function() {
-    this.textContent = 'hit!';
+    var n = Math.floor(Math.random() * 3)
+    this.textContent = n;
   });
   btn.addEventListener('mousedown', function() {
     this.className = 'pushed';
